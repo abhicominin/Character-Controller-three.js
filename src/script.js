f14e29dfe1a0a7bfb0a31311c175d0a13a9591cf
Abhijeet <105330356+abhicominin@users.noreply.github.com>
2023-01-20 10:38:12 +0530

    More Readable Code

--- src/script.js
+++ src/script.js
@@ -1,23 +1,18 @@
-import "./style.css";
-import { KeyDisplay } from "./utils";
-import { CharacterControls } from "./characterControls";
-import { CameraHelper } from "three";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass' 
-import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass'
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass'
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
-import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass'
-import { CopyShader } from 'three/examples/jsm/shaders/CopyShader'
+import { CameraHelper } from "three";
+import { KeyDisplay } from "../../character simulation threejs/src/utils";
+import { CharacterControls } from "../../character simulation threejs/src/characterControls";
+
 
 // SCENE
 var scene = new THREE.Scene();
-scene.fog = new THREE.Fog( 0xFF0032, 10, 50 );
-scene.background = new THREE.Color(0xFF0032);
+scene.fog = new THREE.Fog( 0xFBC252, 10, 50 );
+scene.background = new THREE.Color(0xFBC252);
+
+
+
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(
@@ -29,14 +24,18 @@ var camera = new THREE.PerspectiveCamera(
 camera.position.y = 5;
 camera.position.z = 1;
 camera.position.x = 0;
+scene.add(camera);
+
+
+
 
 //LIGHT
-const dirLight = new THREE.DirectionalLight(0xFF6E31, 0.7)
-dirLight.position.set(- 60, 100, - 10);
+const dirLight = new THREE.DirectionalLight(0xF0997D, 0.8);
+dirLight.position.set(-60, 100, -10);
 dirLight.castShadow = true;
 dirLight.shadow.camera.top = 50;
-dirLight.shadow.camera.bottom = - 50;
-dirLight.shadow.camera.left = - 50;
+dirLight.shadow.camera.bottom = -50;
+dirLight.shadow.camera.left = -50;
 dirLight.shadow.camera.right = 50;
 dirLight.shadow.camera.near = 0.1;
 dirLight.shadow.camera.far = 200;
@@ -44,19 +43,95 @@ dirLight.shadow.mapSize.width = 4096;
 dirLight.shadow.mapSize.height = 4096;
 scene.add(dirLight);
 
-//FLOOR
-const floorgeo = new THREE.PlaneGeometry(1000,1000)
-const floormaterial = new THREE.MeshLambertMaterial({color : 0xCD0404, side : THREE.DoubleSide})
-const floormesh = new THREE.Mesh(floorgeo,floormaterial)
-floormesh.rotation.x = Math.PI/2
-floormesh.receiveShadow = true
-scene.add(floormesh)
+
+
+
+// MODEL LOADER
+
+var characterControls
+
+new GLTFLoader().load("./model/Soldier.glb", function (gltf) {
+    const model = gltf.scene
+    model.traverse( function(object){
+        if(object.isMesh)
+        {
+            object.castShadow = true
+            object.material = new THREE.MeshToonMaterial({ color : 0xffffff, wireframe : false})
+        }
+        scene.add(model)
+    })
+
+    var gltfanimations = gltf.animations // getting all gltf animation clips from gltf model
+    var mixer = new THREE.AnimationMixer(model) // will convert all animation clips into animation actions using mixer which helps into fading in or fading out animations for smooth animations transition
+    var animationmap = new Map()
+
+    gltfanimations.filter( function( a ){ // This function filter out all animations which are not T-Pose
+
+      return a.name != "TPose"
+    }).forEach( function( a ){ // For all of those animations which are filtered out and which are not T-Pose will be converted into AnimationClips
+      
+      animationmap.set(a.name , mixer.clipAction(a))
+    })
+    
+    
+    // INSTANTIATING CHARACTERCONTROLS
+    characterControls = new CharacterControls( model, mixer, animationmap, orbitControls, camera, 'Idle')
+
+})
+
+
+
+
+
+// CONTROL KEYS
+const keypressed = {}
+// UTILITY TO DISPLAY WHICH KEYS ARE PRESSED
+const keyDisplayQueue = new KeyDisplay()
+
+// KEY DOWN EVENT
+document.addEventListener('keydown', function(event) {
+  keyDisplayQueue.down(event.key)
+   if( event.shiftKey && characterControls )
+   {
+    // TOGGLE BETWEEN WALK AND RUN
+    characterControls.switchRunToggle()
+
+   }
+   else
+   {
+     keypressed[event.key.toLowerCase()] = true  // Setting the property of the object true on key press down
+   }
+},false)
+
+
+
+// KEY UP EVENT
+document.addEventListener('keyup', function(event) {
+  keyDisplayQueue.up(event.key)
+   keypressed[event.key.toLowerCase()] = false // Setting the property of the object false on fey press up
+},false)
+
+
+
+
 
 // RENDERER
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+
+// FLOOR
+var floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(1000,1000),
+    new THREE.MeshBasicMaterial({color : 0xFFB100, side : THREE.DoubleSide})
+)
+floor.rotation.x = Math.PI/2
+scene.add(floor)
+
+
 
 // CONTROLS
 var orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -65,110 +140,39 @@ orbitControls.minDistance = 5;
 orbitControls.maxDistance = 15;
 orbitControls.enablePan = false;
 orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
-orbitControls.update();
 
-// MODEL WITH ANIMATIONS
-var characterControls;
-new GLTFLoader().load("./model/Soldier.glb", function (gltf) {
-  var model = gltf.scene;
-  model.traverse(function (object) {
-    if (object.isMesh) 
-    object.castShadow = true;
-    object.material = new THREE.MeshToonMaterial({color : 0xffffff})
-  });
-  scene.add(model);
-  var gltfAnimations = gltf.animations;
-  var mixer = new THREE.AnimationMixer(model);
-  var animationsMap = new Map();
-  gltfAnimations
-    .filter(function (a) {
-      return a.name != "TPose";
-    })
-    .forEach(function (a) {
-      animationsMap.set(a.name, mixer.clipAction(a));
-    });
-  characterControls = new CharacterControls(
-    model,
-    mixer,
-    animationsMap,
-    orbitControls,
-    camera,
-    "Idle"
-  );
-});
 
-// CONTROL KEYS
-var keysPressed = {};
-var keyDisplayQueue = new KeyDisplay();
-document.addEventListener(
-  "keydown",
-  function (event) {
-    keyDisplayQueue.down(event.key);
-    if (event.shiftKey && characterControls) {
-      characterControls.switchRunToggle();
-    } else {
-      keysPressed[event.key.toLowerCase()] = true;
-    }
-  },
-  false
-);
-document.addEventListener(
-  "keyup",
-  function (event) {
-    keyDisplayQueue.up(event.key);
-    keysPressed[event.key.toLowerCase()] = false;
-  },
-  false
-);
 
-//CLOCK
+
+// CLOCK
 var clock = new THREE.Clock();
 
-//POST-PROCESSING
-// Creating a effectcomposer
-const composer = new EffectComposer(renderer)
-// First pass as a RenderPass to render our scene with our camera into the first render target.
-composer.addPass(new RenderPass(scene,camera))
-
-//adding the FilmPass
-const filmpass = new FilmPass(
-  0.35,
-  0.025,
-  648,
-  false
-)
-filmpass.renderToScreen = false
-composer.addPass(filmpass)
-
-//adding the GlitchPass
-const glitchpass = new GlitchPass()
-glitchpass.renderToScreen = true
-composer.addPass(glitchpass)
-
-
-
-
-
-// ANIMATE
+// RENDER FUNCTION
 function animate() {
-  var mixerUpdateDelta = clock.getDelta();
-  if (characterControls) {
-    characterControls.update(mixerUpdateDelta, keysPressed);
+
+  let mixerUpdateDelta = clock.getDelta()
+   
+  if( characterControls )
+  {
+    characterControls.update( mixerUpdateDelta , keypressed ) // Passing the delta and keypressed information to the update method of CharacterControls
   }
+
+
   orbitControls.update();
-  composer.render(mixerUpdateDelta)
-  //renderer.render(scene, camera);
+  renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
-document.body.appendChild(renderer.domElement);
 animate();
 
-// RESIZE HANDLER
+
+
+
+//RESIZE EVENT
+window.addEventListener("resize", onWindowResize);
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  composer.setSize(window.innerWidth,innerHeight)
-  //renderer.setSize(window.innerWidth, window.innerHeight);
-  keyDisplayQueue.updatePosition();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener("resize", onWindowResize);
+
